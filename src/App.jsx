3bd6088235bca9ca1bc6e968f89522cf74fd98f6
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-04-10 17:06:44 -0700

    Added data which will be iterated over with .map() in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import TravelCard from './components/travelCard'
+import data from './data'
 
 function App() {
 
   return (
-    <>
-      <h1>This is a title~!</h1>
-    </>
+    <div className="app">
+      <header>
+        <i className="fa-solid fa-earth-americas fa-xl"></i><h1 className="header-text">my travel journal.</h1>
+      </header>
+      <main className='travel-card-wrapper'>
+        {data.map((data, index) => {
+          return <TravelCard key={index} data={data} />
+        })}
+      </main>
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: "Point Reyes Lighthouse",
+    location: "Point Reyes, CA",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/37%C2%B059'47.1%22N+123%C2%B001'15.4%22W/@37.9962703,-123.0213306,195m/data=!3m1!1e3!4m4!3m3!8m2!3d37.9964167!4d-123.0209444?entry=ttu",
+    date: "December 2015",
+    startDate: "",
+    endDate: "",
+    description:
+      "In 2015 I went on a road trip from Sacramento to San Francisco by taking the scenic route. I traveled through Clear Lake to Fort Bragg and slept in my car. After visiting Glass Beach at Sunrise, I began the almost 5 hour drive from Fort Bragg to San Francisco via CA-1, California's coastal highway. I stopped at two lighthouses along the way in Point Arena and Point Reyes. Point Arena was nice, but Point Reyes was the one I was really excited(read: nostalgic) for, as I only had vague memories of it from going there when I was very young. Even though I was tired from driving, sunlight dwindling, and still more than an hour from SF, I made the twenty minute detour to visit Point Reyes. It was a hike from the parking lot, but, completely unplanned, I reached the lookout point exactly in the middle of golden hour and took the photo you see here. One of the most beautiful experiences I've had.",
+    imageUrl: "/images/point-reyes.jpg",
+  },
+  {
+    title: "Evolution Lake",
+    location: "Bishop, CA - Kings Canyon National Park",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/37%C2%B010'23.5%22N+118%C2%B041'50.9%22W/@37.1731798,-118.697458,702m/data=!3m1!1e3!4m4!3m3!8m2!3d37.1731798!4d-118.697458?entry=ttu",
+    date: "August 2015",
+    startDate: "",
+    endDate: "",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, recusandae tempore distinctio omnis est, unde eos quod atque, fugiat necessitatibus illum aspernatur autem repellendus. Minima dolores ea corrupti libero ex porro nemo veritatis doloribus eaque reiciendis nostrum magni accusamus consequatur, cupiditate eius, beatae ipsum vitae iste amet necessitatibus omnis enim commodi doloremque? Dicta, porro? Sapiente dolore facere dignissimos consequatur, officia, aut asperiores autem natus impedit distinctio optio, recusandae expedita voluptate adipisci ducimus minima sed vitae.",
+    imageUrl: "/images/evolution-lake.jpg",
+  },
+  {
+    title: "New Brighton Beach",
+    location: "Santa Cruz, CA",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/36%C2%B058'43.3%22N+121%C2%B056'10.0%22W/@36.9787035,-121.9361279,853m/data=!3m1!1e3!4m4!3m3!8m2!3d36.9786947!4d-121.9361028?entry=ttu",
+    date: "April 2018",
+    startDate: "",
+    endDate: "",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, recusandae tempore distinctio omnis est, unde eos quod atque, fugiat necessitatibus illum aspernatur autem repellendus. Minima dolores ea corrupti libero ex porro nemo veritatis doloribus eaque reiciendis nostrum magni accusamus consequatur, cupiditate eius, beatae ipsum vitae iste amet necessitatibus omnis enim commodi doloremque? Dicta, porro? Sapiente dolore facere dignissimos consequatur, officia, aut asperiores autem natus impedit distinctio optio, recusandae expedita voluptate adipisci ducimus minima sed vitae.",
+    imageUrl: "/images/new-brighton-beach.jpg",
+  },
+  {
+    title: "Lobos Valley Overlook",
+    location: "San Francisco, CA",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/37%C2%B047'16.2%22N+122%C2%B028'32.5%22W/@37.7874809,-122.4758658,435m/data=!3m1!1e3!4m4!3m3!8m2!3d37.7878362!4d-122.4756795?entry=ttu",
+    date: "April 2018",
+    startDate: "",
+    endDate: "",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima, recusandae tempore distinctio omnis est, unde eos quod atque, fugiat necessitatibus illum aspernatur autem repellendus. Minima dolores ea corrupti libero ex porro nemo veritatis doloribus eaque reiciendis nostrum magni accusamus consequatur, cupiditate eius, beatae ipsum vitae iste amet necessitatibus omnis enim commodi doloremque? Dicta, porro? Sapiente dolore facere dignissimos consequatur, officia, aut asperiores autem natus impedit distinctio optio, recusandae expedita voluptate adipisci ducimus minima sed vitae.",
+    imageUrl: "/images/san-francisco-2.jpg",
+  },
+];
